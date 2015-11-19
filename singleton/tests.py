1d8b4e3294e828402391109995b7f1ab62eddab7
Stephen <stephen@britecore.com>
2015-11-19 11:59:35 -0600

    update singleton tests

--- singleton/tests.py
+++ singleton/tests.py
@@ -7,9 +7,9 @@ class TestSingleton(unittest.TestCase):
         self.singleton_1 = Singleton(10)
         self.singleton_2 = Singleton(50)
     
-    def test_singleton_data_does_not_change(self):
-        self.assertEqual(self.singleton_1.data, 10)
-        self.assertEqual(self.singleton_2.data, 10)
+    def test_singleton_data_does_update(self):
+        self.assertEqual(self.singleton_1.data, 50)
+        self.assertEqual(self.singleton_2.data, 50)
         
     def test_singleton_is_singleton(self):
         self.assertEqual(self.singleton_1.data, self.singleton_2.data)
